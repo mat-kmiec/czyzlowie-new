5b19ebb3c47ff155197cbb99445c598fba7d27d5
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-05-05 09:14:29 +0200

    Add Dockerfile and configure backend with DB

---
+++ czyzlowie-backend/src/main/java/pl/czyzlowie/CzyzlowieApplication.java
@@ -0,0 +1,13 @@
+package pl.czyzlowie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzyzlowieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzyzlowieApplication.class, args);
+    }
+
+}
---
+++ czyzlowie-backend/src/test/java/pl/czyzlowie/CzyzlowieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.czyzlowie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzyzlowieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
